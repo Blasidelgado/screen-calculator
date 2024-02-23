d1beface84f257c763d263bb6a3cf68c4e22f3d1
Blasidelgado <blasidelgado@gmail.com>
2024-02-23 17:07:53 -0300

    fix divide function

--- script.js
+++ script.js
@@ -13,16 +13,26 @@ const multiply = (num1, num2) => {
 }
 
 const divide = (num1, num2) => {
-    if (num2 === 0) {
-        console.log("Cannot divide by zero");
-        return num1;
-    }
-
     return num1 / num2;
 }
 
 const operate = (num1, num2, oper) => {
-
+    switch(oper) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return substract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            if (num2 === 0){
+                console.log("Cannot divide by zero");
+                return num1;
+            }
+            return divide(num1, num2);
+        default:
+            return console.log("Clear function should be called");
+    }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
