ad5939a901b7ab39ec65e3b0d37c4805f1b9a7f1
Blasidelgado <blasidelgado@gmail.com>
2024-02-23 16:50:26 -0300

    Add main arithmetical functions

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-document.addEventListener('DOMContentLoaded', () => console.log("Scripts loaded correctly"));
+const add = (num1, num2) => {
+    return num1 + num2;
+}
+
+const substract = (num1, num2) => {
+    return num1 - num2;
+}
+
+const multiply = (num1, num2) => {
+    return num1 * num2;
+}
+
+const divide = (num1, num2) => {
+    return num2 !== 0 ? num1 / num2 : "Cannot divide by zero";
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Scripts loaded correctly")
+});
